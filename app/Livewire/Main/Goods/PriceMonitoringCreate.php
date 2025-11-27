8bd8a9596f90368fd56aa41931b54814bf443d6d
Bellie Joe Jandusay <jandusayjoe14@gmail.com>
2025-11-27 16:26:45 +0800

    asdastw wqe

---
+++ app/Livewire/Main/Goods/PriceMonitoringCreate.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Main\Goods;
+
+use Livewire\Component;
+
+class PriceMonitoringCreate extends Component
+{
+    public function render()
+    {
+        return view('livewire.main.goods.price-monitoring-create');
+    }
+}
---
+++ app/Livewire/Main/Goods/PriceMonitoringEdit.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Main\Goods;
+
+use Livewire\Component;
+
+class PriceMonitoringEdit extends Component
+{
+    public function render()
+    {
+        return view('livewire.main.goods.price-monitoring-edit');
+    }
+}
