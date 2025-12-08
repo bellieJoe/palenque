e479de96eb8a85d3da10dce2ff4575de01586599
Bellie Joe Jandusay <jandusayjoe14@gmail.com>
2025-12-08 16:21:37 +0800

    sdfsdf

---
+++ app/Livewire/Dashboard/VendorDashboard.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Livewire\Dashboard;
+
+use Illuminate\Support\Carbon;
+use Livewire\Component;
+use Livewire\WithoutUrlPagination;
+use Livewire\WithPagination;
+
+class VendorDashboard extends Component
+{
+    use WithoutUrlPagination, WithPagination;
+    public $startFilter;
+    public $endFilter;
+
+    public function updatingStartFilter()
+    {
+        $this->resetPage();
+    }
+
+    public function updatingEndFilter()
+    {
+        $this->resetPage();
+    }
+
+    public function mount()
+    {
+        $this->startFilter = Carbon::parse(date('Y')."-01-01")->format('Y-m-d');
+        $this->endFilter = Carbon::parse(date('Y')."-12-31")->format('Y-m-d'); 
+    }
+    
+    public function render()
+    {
+        return view('livewire.dashboard.vendor-dashboard');
+    }
+}
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,4 +1,5 @@
 <x-layouts.app :title="__('Dashboard')">
     @livewire('dashboard.admin-dashboard')
     @livewire('dashboard.market-dashboard')
+    @livewire('dashboard.vendor-dashboard')
 </x-layouts.app>
