e98e27b66b2d51a6d82958cbb759d6976265b175
Bellie Joe Jandusay <jandusayjoe14@gmail.com>
2025-12-28 20:04:33 +0800

    Implemented the uploading of vendor attachments

--- app/Livewire/Settings/Profile/ProfileIndex.php
+++ app/Livewire/Settings/Profile/ProfileIndex.php
@@ -2,12 +2,58 @@
 
 namespace App\Livewire\Settings\Profile;
 
+use App\Models\Vendor;
+use Illuminate\Support\Facades\Log;
+use Livewire\Attributes\Validate;
 use Livewire\Component;
+use Livewire\WithFileUploads;
+
+use function Flasher\Notyf\Prime\notyf;
 
 class ProfileIndex extends Component
 {
+    use WithFileUploads;
+
+    #[Validate('required|file|mimetypes:image/jpeg,image/png,application/pdf|max:2048')]
+    public $businessPermit;
+    #[Validate('required|file|mimetypes:image/jpeg,image/png,application/pdf|max:2048')]
+    public $dtiPermit;
     public function render()
     {
         return view('livewire.settings.profile.profile-index');
+    }
+
+    public function saveBusinessPermit()
+    {
+        try {
+            $this->validateOnly('businessPermit');
+            $fileName = time() . '.' . $this->businessPermit->getClientOriginalExtension();
+            $this->businessPermit->storeAs('business_permit', $fileName, 'public');
+            Vendor::where('user_id', auth()->user()->id)->update([
+                'business_permit' => $fileName
+            ]);
+            // notyf()->success('Business Permit Updated');
+            return $this->redirect(request()->header('Referer'));
+        } catch (\Throwable $th) {
+            Log::error($th);
+        }
+        
+    }
+
+    public function saveDtiPermit()
+    {
+        try {
+            $this->validateOnly('dtiPermit');
+            $fileName = time() . '.' . $this->dtiPermit->getClientOriginalExtension();
+            $this->dtiPermit->storeAs('dti_permit', $fileName, 'public');
+            Vendor::where('user_id', auth()->user()->id)->update([
+                'dti_permit' => $fileName
+            ]);
+            // notyf()->success('Business Permit Updated');
+            return $this->redirect(request()->header('Referer'));
+        } catch (\Throwable $th) {
+            Log::error($th);
+        }
+        
     }
 }
--- resources/views/livewire/settings/profile/profile-index.blade.php
+++ resources/views/livewire/settings/profile/profile-index.blade.php
@@ -32,8 +32,29 @@
                                 <p>{{ $vendor->representative_name }}</p>
                             </div>
                             <div class="col-12 col-lg-4">
-                                <small>Name</small>
+                                <small>Contact Number</small>
                                 <p>{{ $vendor->contact_number }}</p>
+                            </div>
+                        </div>
+                        <h4 >Attachments</h4>
+                        <div class="row">
+                            <div class="col-12 col-lg-4">
+                                <small>Business/Mayor's Permit</small>
+                                <p>
+                                    <a href="{{ Storage::url('business_permit/'.$vendor->business_permit) }}" target="_blank">
+                                        Click to View
+                                    </a>
+                                    <button class="btn btn-sm btn-outline-secondary" data-toggle="modal" data-target="#uploadBusinessPermitModal"><i class="fa-regular fa-pen-to-square"></i></button>
+                                </p>
+                            </div>
+                            <div class="col-12 col-lg-4">
+                                <small>DTI Permit</small>
+                                <p>
+                                    <a href="{{ Storage::url('dti_permit/'.$vendor->dti_permit) }}" target="_blank">
+                                        Click to View
+                                    </a>
+                                    <button class="btn btn-sm btn-outline-secondary" data-toggle="modal" data-target="#uploadDtiPermitModal" ><i class="fa-regular fa-pen-to-square"></i></button>
+                                </p>
                             </div>
                         </div>
                     </div>
@@ -41,4 +62,55 @@
             </div>
         @endif
     </div>
+    <div id="uploadBusinessPermitModal" class="modal fade" wire:ignore.self>
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">Upload Business Permit</h5>
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                </div>
+                <div class="modal-body">
+                    <div class="mb-3">
+                        <label>Business Permit</label>
+                        <input type="file"
+                            class="form-control"
+                            accept=".pdf,.jpg,.png"
+                            wire:model.live="businessPermit">
+                        @error('businessPermit') <span class="text-danger">{{ $message }}</span> @enderror
+                        <div wire:loading wire:target="businessPermit">Uploading...</div>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary" wire:click="saveBusinessPermit" wire:loading.attr="disabled">Save</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div id="uploadDtiPermitModal" class="modal fade" wire:ignore.self>
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">Upload DTI Permit</h5>
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                </div>
+                <div class="modal-body">
+                    <div class="mb-3">
+                        <label>DTI Permit</label>
+                        <input type="file"
+                            class="form-control"
+                            accept=".pdf,.jpg,.png"
+                            wire:model.live="dtiPermit">
+                        @error('dtiPermit') <span class="text-danger">{{ $message }}</span> @enderror
+                        <div wire:loading wire:target="dtiPermit">Uploading...</div>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary" wire:click="saveDtiPermit" wire:loading.attr="disabled">Save</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
 </div>
