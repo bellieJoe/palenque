ea5efe600df7f78c6fbc0e747a279a81e8b976f1
Bellie Joe Jandusay <jandusayjoe14@gmail.com>
2026-01-16 14:04:01 +0800

    Price monitoring in vendor

--- app/Livewire/Vendor/PriceMonitoring/PriceMonitoringIndex.php
+++ app/Livewire/Vendor/PriceMonitoring/PriceMonitoringIndex.php
@@ -2,12 +2,96 @@
 
 namespace App\Livewire\Vendor\PriceMonitoring;
 
+use App\Models\Item;
+use App\Models\VendorPrice;
 use Livewire\Component;
+use Livewire\WithPagination;
+use Livewire\WithoutUrlPagination;
+
+use function Flasher\Notyf\Prime\notyf;
 
 class PriceMonitoringIndex extends Component
 {
+    use WithPagination, WithoutUrlPagination;
+
+    public $date;
+    public $search;
+    public $items = [];
+
+    public function mount()
+    {
+        $this->date = now()->toDateString();
+
+        $this->loadPrices();
+    }
+
+    public function updatedDate()
+    {
+        $this->loadPrices();
+    }
+
+    public function updatedSearch()
+    {
+        $this->resetPage();
+    }
+
+    /**
+     * Catch ALL item price updates
+     */
+    public function updated($name, $value)
+    {
+        if (! str_starts_with($name, 'items.')) {
+            return;
+        }
+
+        $itemId = str_replace('items.', '', $name);
+
+        VendorPrice::updateOrCreate(
+            [
+                'vendor_id' => auth()->user()->vendor->id,
+                'item_id' => $itemId,
+                'date' => $this->date,
+                'municipal_market_id' => auth()->user()->marketDesignation()->id,
+            ],
+            [
+                'price' => $value ?? 0,
+            ]
+        );
+        notyf()->position('y', 'top')->success('Price updated successfully!');
+    }
+
+    /**
+     * Load prices ONLY when needed
+     */
+    protected function loadPrices()
+    {
+        $prices = VendorPrice::where('vendor_id', auth()->user()->vendor->id)
+            ->whereDate('date', $this->date)
+            ->get()
+            ->keyBy('item_id');
+
+        $items = Item::where('municipal_market_id', auth()->user()->marketDesignation()->id)
+            ->get();
+
+        $this->items = [];
+
+        foreach ($items as $item) {
+            $this->items[$item->id] = $prices[$item->id]->price ?? 0;
+        }
+    }
+
     public function render()
     {
-        return view('livewire.vendor.price-monitoring.price-monitoring-index');
+        return view('livewire.vendor.price-monitoring.price-monitoring-index', [
+            'wetGoods' => Item::where('type', 'WET')
+                ->where('municipal_market_id', auth()->user()->marketDesignation()->id)
+                ->where('name', 'like', "%{$this->search}%")
+                ->get(),
+
+            'dryGoods' => Item::where('type', 'DRY')
+                ->where('municipal_market_id', auth()->user()->marketDesignation()->id)
+                ->where('name', 'like', "%{$this->search}%")
+                ->get(),
+        ]);
     }
 }
--- resources/views/livewire/vendor/price-monitoring/price-monitoring-index.blade.php
+++ resources/views/livewire/vendor/price-monitoring/price-monitoring-index.blade.php
@@ -1,11 +1,101 @@
 <div>
     <x-page-header title="Price Monitoring" />
-    <div class="card">
+
+    <div class="row mb-3 justify-content-end">
+        <div class=" col col-md-6 col-lg-4">
+            <input
+                type="date"
+                class="form-control"
+                wire:model.live.debounce.300ms="date"
+            >
+        </div>
+        <div class=" col col-md-6 col-lg-4">
+            <input
+                type="text"
+                class="form-control"
+                placeholder="Search Item"
+                wire:model.live.debounce.300ms="search"
+            >
+        </div>
+    </div>
+
+    {{-- WET GOODS --}}
+    <div class="card mb-3">
         <div class="card-header">
-            <h6 class="card-title">Price Monitoring</h6>
+            <h6 class="card-title mb-0">Wet Goods</h6>
         </div>
         <div class="card-body">
-            <livewire:vendor.price-monitoring.price-monitoring-index />
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>Item</th>
+                        <th width="200">Price</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @forelse ($wetGoods as $item)
+                        <tr>
+                            <td>{{ $item->name }}</td>
+                            <td>
+                                <div class="input-group">
+                                    <div class="input-group-prepend">
+                                        <span class="input-group-text">PHP</span>
+                                    </div>
+                                    <input
+                                        type="number"
+                                        class="form-control"
+                                        wire:model.live.debounce.500ms="items.{{ $item->id }}"
+                                    >
+                                </div>
+                            </td>
+                        </tr>
+                    @empty
+                        <tr>
+                            <td colspan="2">No items found</td>
+                        </tr>
+                    @endforelse
+                </tbody>
+            </table>
         </div>
-    <div>
+    </div>
+
+    {{-- DRY GOODS --}}
+    <div class="card mb-3">
+        <div class="card-header">
+            <h6 class="card-title mb-0">Dry Goods</h6>
+        </div>
+        <div class="card-body">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>Item</th>
+                        <th width="200">Price</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @forelse ($dryGoods as $item)
+                        <tr>
+                            <td>{{ $item->name }}</td>
+                            <td>
+                                <div class="input-group">
+                                    <div class="input-group-prepend">
+                                        <span class="input-group-text">PHP</span>
+                                    </div>
+                                    <input
+                                        type="number"
+                                        class="form-control"
+                                        wire:model.live.debounce.500ms="items.{{ $item->id }}"
+                                    >
+                                </div>
+                            </td>
+                        </tr>
+                    @empty
+                        <tr>
+                            <td colspan="2">No items found</td>
+                        </tr>
+                    @endforelse
+                </tbody>
+            </table>
+        </div>
+    </div>
 </div>
