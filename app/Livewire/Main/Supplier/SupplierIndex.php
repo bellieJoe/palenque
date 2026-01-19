94688a929dc1454bbd7350825156637b3046ea83
Bellie Joe Jandusay <jandusayjoe14@gmail.com>
2026-01-19 21:20:34 +0800

    sdfsdafihusdf

--- app/Livewire/Main/Supplier/SupplierIndex.php
+++ app/Livewire/Main/Supplier/SupplierIndex.php
@@ -29,6 +29,9 @@ class SupplierIndex extends Component
             notyf()->position('y', 'top')->error('Cannot delete supplier with existing deliveries!');
             return;
         }
+        Supplier::where('id', $id)->update([
+            'restore_date' => now()->addDays(60)->format('Y-m-d')
+        ]);
         Supplier::where("id", $id)->delete();
         notyf()->position('y', 'top')->success('Supplier deleted successfully!');
         $this->dispatch('refresh-suppliers');
@@ -45,15 +48,59 @@ class SupplierIndex extends Component
         //     notyf()->position('y', 'top')->error('Cannot delete origin with existing suppliers!');
         //     return;
         // }
+        Origin::find($id)
+        ->update([
+            'restore_date' => now()->addDays(60)->format('Y-m-d')
+        ]);
+
         Origin::find($id)->delete();
         notyf()->position('y', 'top')->success('Origin deleted successfully!');
         $this->dispatch('refresh-suppliers');
     }
 
+    public function restoreOrigin($id){
+        Origin::withTrashed()->find($id)->update(['restore_date' => null]);
+        Origin::withTrashed()->find($id)->restore();
+        notyf()->position('y', 'top')->success('Origin restored successfully!');
+    }
+
+    public function restoreSupplier($id){
+        Supplier::withTrashed()->find($id)->update(['restore_date' => null]);
+        Supplier::withTrashed()->find($id)->restore();
+        notyf()->position('y', 'top')->success('Supplier restored successfully!');
+    }
+
     public function render()
     {
-        $suppliers = Supplier::query()->where('name', 'like', '%' . $this->search . '%')->where('municipal_market_id', auth()->user()->marketDesignation()->id)->paginate(10);
-        $origins = Origin::where("municipal_market_id", auth()->user()->marketDesignation()->id)->get();
+        $suppliers = Supplier::query()
+        ->withTrashed()
+        ->with('origin', function($q){
+            $q->withTrashed();
+        })
+        ->where(function ($q) {
+            $q->whereNull('deleted_at') // active records (ignore restore_date)
+            ->orWhere(function ($q) {
+                $q->whereNotNull('deleted_at') // deleted records
+                    ->whereDate('restore_date', '>', today());
+            });
+        })
+        ->where('name', 'like', '%' . $this->search . '%')
+        ->where('municipal_market_id', auth()->user()->marketDesignation()->id)
+        ->orderBy('restore_date', 'asc')
+        ->paginate(10);
+        $origins = Origin::query()
+                ->where('municipal_market_id', auth()->user()->marketDesignation()->id)
+                ->where(function ($q) {
+                    $q->whereNull('deleted_at') // active records (ignore restore_date)
+                    ->orWhere(function ($q) {
+                        $q->whereNotNull('deleted_at') // deleted records
+                            ->whereDate('restore_date', '>', today());
+                    });
+                })
+                ->withTrashed()
+                ->orderBy('restore_date', 'asc')
+                ->get();
+
         return view('livewire.main.supplier.supplier-index', [
             'suppliers' => $suppliers,
             'origins' => $origins
--- resources/views/livewire/main/supplier/supplier-index.blade.php
+++ resources/views/livewire/main/supplier/supplier-index.blade.php
@@ -41,8 +41,13 @@
                             <tr>
                                 <td>{{ $origin->name }} ({{ $origin->is_local ? "Local" : "Import" }})</td>
                                 <td>
-                                    <button class="btn btn-outline-danger" wire:click="deleteOrigin({{$origin->id}})" wire:confirm="Are you sure you want to delete this origin?">Delete</button>
-                                    <a class="btn btn-outline-primary" href="{{ route("main.origins.edit" , $origin->id) }}">Edit</a>
+                                    @if ($origin->restore_date == null && $origin->deleted_at == null)
+                                        <button class="btn btn-outline-danger" wire:click="deleteOrigin({{$origin->id}})" wire:confirm="Are you sure you want to delete this origin?">Delete</button>
+                                        <a class="btn btn-outline-primary" href="{{ route("main.origins.edit" , $origin->id) }}">Edit</a>
+                                    @endif
+                                    @if ($origin->restore_date  && $origin->restore_date > today())
+                                        <button class="btn btn-outline-danger" wire:click="restoreOrigin({{$origin->id}})" wire:confirm="Are you sure you want to restore this origin?">Restore ({{ number_format(now()->diffInDays($origin->restore_date), 0) }} days left)</button>
+                                    @endif
                                 </td>
                             </tr>
                         @empty
@@ -74,8 +79,13 @@
                                 <td>{{ $supplier->contact_number }}</td>
                                 <td>{{ $supplier->email }}</td>
                                 <td>
-                                    <button class="btn btn-outline-danger" wire:click="deleteSupplier({{$supplier}})" wire:confirm="Are you sure you want to delete this post?">Delete</button>
-                                    <button class="btn btn-outline-primary" wire:click="editSupplier({{$supplier->id}})">Edit</button>
+                                    @if ($supplier->deleted_at == null && $supplier->restore_date == null)
+                                        <button class="btn btn-outline-danger" wire:click="deleteSupplier({{$supplier->id}})" wire:confirm="Are you sure you want to delete this supplier?">Delete</button>
+                                        <button class="btn btn-outline-primary" wire:click="editSupplier({{$supplier->id}})">Edit</button>
+                                    @endif
+                                    @if ($supplier->deleted_at && $supplier->restore_date  && $supplier->restore_date > today())
+                                        <button class="btn btn-outline-danger" wire:click="restoreSupplier({{$supplier->id}})" wire:confirm="Are you sure you want to restore this supplier?">Restore ({{ number_format(now()->diffInDays($supplier->restore_date), 0) }} day/s left)</button>
+                                    @endif
                                 </td>
                             </tr>
                         @empty
